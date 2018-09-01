1842e934cf29056fe0217a9e2bf38c11ace51b24
Tobibur Rahman <tobiburrahman786@gmail.com>
2018-09-01 12:40:37 +0530

    Added okhttp timeout

--- app/src/main/java/com/tobibur/swipequotes/model/service/ApiClient.kt
+++ app/src/main/java/com/tobibur/swipequotes/model/service/ApiClient.kt
@@ -5,26 +5,33 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
+import java.util.concurrent.TimeUnit
 
 
 class ApiClient{
 
     private val baseURL = "http://api.forismatic.com/"
     private var retrofit : Retrofit? = null
+    private val REQUEST_TIMEOUT = 60
 
     private var logging = HttpLoggingInterceptor()
 
-    private fun getHttpLogClient() : OkHttpClient{
+    private fun getHttpLogClient(): OkHttpClient {
+        val httpClient = OkHttpClient().newBuilder()
+                .connectTimeout(REQUEST_TIMEOUT.toLong(), TimeUnit.SECONDS)
+                .readTimeout(REQUEST_TIMEOUT.toLong(), TimeUnit.SECONDS)
+                .writeTimeout(REQUEST_TIMEOUT.toLong(), TimeUnit.SECONDS)
+
         if (BuildConfig.DEBUG) {
             logging.level = HttpLoggingInterceptor.Level.BODY
         }
-        return OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build()
+        httpClient.addInterceptor(logging)
+        return httpClient.build()
     }
 
     fun getClient(): Retrofit? {
+        //if(okHttpClient == null) initOkHttp()
+
         if (retrofit == null) {
             retrofit = Retrofit.Builder()
                     .baseUrl(baseURL)
